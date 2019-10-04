3b396b5500de1f22bc46a239740840efb3e14867
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-04 07:57:26 +0200

    1.1 - change of property

---
+++ JavascriptandtheDOM/TweakanElement/main.js
@@ -0,0 +1,8 @@
+// 1.0 - Tweak an Element
+
+/*
+ 1.1 - Use the .innerHTML property to modify the content of the body to display the text ‘This is the text of the body element’.
+
+Notice that the previous content inside of body has been replaced
+*/
+document.body.innerHTML = 'This is the text of the body element';
