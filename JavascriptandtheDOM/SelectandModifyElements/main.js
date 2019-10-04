15eb2e7157f4ab8be4d4722bb6fc422af6018576
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-04 09:43:39 +0200

    3.1 - correction

--- JavascriptandtheDOM/SelectandModifyElements/main.js
+++ JavascriptandtheDOM/SelectandModifyElements/main.js
@@ -1,5 +1,9 @@
 /*
-1.1
+3.1
 Call the .querySelector() method to select the tag of h1 from the DOM. Access that element’s .innerHTML property to change the h1 title to ‘Most popular TV show searches in 2016’.
 */
 document.querySelector('h1').innerHTML ='Most popular TV show searches in 2016';
+
+/*
+3.2
+*/
