cdd9878321e58ab08909ba29defec73669459574
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-04 11:45:43 +0200

    5.1 - add createElement 'li'

--- JavascriptandtheDOM/CreateAndInsertElements/main.js
+++ JavascriptandtheDOM/CreateAndInsertElements/main.js
@@ -1,1 +1,14 @@
 // 5.0 - Create and Insert Elements
+
+/*
+5.1
+
+  a) Create a li element in the document and save it to a variable.
+    
+  b) Assign the new element an id of "oaxaca" by using the .id property			on the new variable.
+  
+  c) Assign the element the text “Oaxaca, Mexico” by using the							.innerHTML method.
+*/
+let listTag = document.createElement('li');
+listTag.id = "oaxaca";
+listTag.innerHTML("Oaxaca, Mexico");
