51da64556053f80e21d089830d01313e7a963e5c
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-04 10:31:01 +0200

    4.1 - backgroundColor

--- JavascriptandtheDOM/Styleanelement/main.js
+++ JavascriptandtheDOM/Styleanelement/main.js
@@ -1,1 +1,7 @@
 // 4.0 - Style an element
+
+/*
+4.1
+Style the background-color of the body in the blog-post document to match the color of the Codecademy text editor, #201F2E by using the .style property.
+*/
+document.body.style.backgroundColor = "#201F2E";
