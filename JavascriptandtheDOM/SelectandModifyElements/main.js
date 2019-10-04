77224967e5f8eab9c60d5ad96ed301a6fabae334
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-04 09:45:49 +0200

    3.2 - add getElementById

--- JavascriptandtheDOM/SelectandModifyElements/main.js
+++ JavascriptandtheDOM/SelectandModifyElements/main.js
@@ -6,4 +6,6 @@ document.querySelector('h1').innerHTML ='Most popular TV show searches in 2016';
 
 /*
 3.2
+Use the .getElementById method to access the element with an id of "fourth". Access that element’s .innerHTML to replace the content in the fourth item in the list to read “Fourth element”. 
 */
+document.getElementById("fourth").innerHTML = "Fourth element";
