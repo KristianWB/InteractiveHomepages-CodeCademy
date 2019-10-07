040b88a0febc549c136ee13431e30d6678748c77
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-07 10:11:04 +0200

    8.2 - select parantNode

--- JavascriptandtheDOM/TraversingtheDOM/main.js
+++ JavascriptandtheDOM/TraversingtheDOM/main.js
@@ -7,3 +7,9 @@ Access the .firstChild of the body and save it to a variable named first. Modify
 let first = document.body.firstChild;
 
 first.innerHTML = 'I am the child!';
+
+/*
+8.2
+Use the .parentNode property to access the parent element of the variable first and modify its .innerHTML to say ‘I am the parent and my inner HTML has been replaced!’ Take a moment to notice the change in the web page. 
+*/
+first.parentNode.innerHTML = 'I am the parent and my inner HTML has been replaced!';
