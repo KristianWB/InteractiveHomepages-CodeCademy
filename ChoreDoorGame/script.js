e9817ce5baba252bb10dc3decce9e11bc6562570
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-07 11:26:00 +0200

    1.7 - getElementById

--- ChoreDoorGame/script.js
+++ ChoreDoorGame/script.js
@@ -1,1 +1,7 @@
 // 1.0 - project start - Getting Started - Get a Door, Open a Door!
+
+/*
+1.7
+Navigate to the script.js file. Create a global variable called doorImage1. Use a JavaScript DOM method to assign this global variable to the HTML element with the id of door1.
+*/
+let doorImage1 = document.getElementById('door1');
