9e849664949c6319b7392f6e177eced8ef8d1128
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-07 11:28:13 +0200

    1.8 - onClick

--- ChoreDoorGame/script.js
+++ ChoreDoorGame/script.js
@@ -5,3 +5,11 @@
 Navigate to the script.js file. Create a global variable called doorImage1. Use a JavaScript DOM method to assign this global variable to the HTML element with the id of door1.
 */
 let doorImage1 = document.getElementById('door1');
+
+/*
+1.8
+Inside the script.js file, underneath your global variable, assign doorImage1.onclick to a new, empty arrow function.
+
+This function will run whenever the first door image element is clicked.
+*/
+doorImage1.onClick = () => {}
