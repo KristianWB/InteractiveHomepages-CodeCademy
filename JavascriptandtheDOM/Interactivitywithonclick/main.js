7948f9d759f3641eab5fe08c5a65fc1c58c92add
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-07 09:52:03 +0200

    7.1 - changing properties

--- JavascriptandtheDOM/Interactivitywithonclick/main.js
+++ JavascriptandtheDOM/Interactivitywithonclick/main.js
@@ -1,8 +1,17 @@
-// 7.0 - Interactivity with onclick
-
 let element = document.querySelector("button");
 
 function turnButtonRed (){
+  
+  /*
+  7.0 
+  Modify the body of the turnButtonRed() function so that it modifies the button as follows:
 
+    - Assigns the .style.backgroundColor to 'red'
+   -  Assigns the style.color to 'white'
+   -  Modifies the .innerHTML to 'Red Button'
+  */
+	element.style.backgroundColor = 'red';
+  element.style.color = 'white';
+  element.innerHTML = 'Red Button';
 }
 
