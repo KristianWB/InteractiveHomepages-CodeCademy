3d0112eb1741fd5efc616a0de38497a79daddd2c
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-07 10:08:45 +0200

    8.1 - select element by property

--- JavascriptandtheDOM/TraversingtheDOM/main.js
+++ JavascriptandtheDOM/TraversingtheDOM/main.js
@@ -1,1 +1,9 @@
 // 8.0 - Traversing the DOM
+
+/*
+8.1
+Access the .firstChild of the body and save it to a variable named first. Modify its .innerHTML to say ‘I am the child!’ Take a moment to note which element has now been modified.
+*/
+let first = document.body.firstChild;
+
+first.innerHTML = 'I am the child!';
