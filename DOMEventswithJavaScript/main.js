1f997b6e56e7091270f32833382e5221451e5b63
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-09 07:44:51 +0200

    #3 - Event Handler Registration

---
+++ DOMEventswithJavaScript/main.js
@@ -0,0 +1,12 @@
+//# - 
+
+let readMore = document.getElementById('read-more')
+let moreInfo = document.getElementById('more-info')
+
+// Write your code here:
+//#3.1 - write eventHandler
+
+readMore.onclick = function (){
+  
+}
+
