f734bf7813f7edc367674f37507c1a0843141362
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-09 08:09:47 +0200

    #4 - finished

--- DOMEventswithJavaScript/main-1.js
+++ DOMEventswithJavaScript/main-1.js
@@ -1,5 +1,3 @@
-//#4.1 - 
-
 // Add the code you want to test below:
 let view = document.getElementById('view-button');
 let close = document.getElementById('close-button');
@@ -15,7 +13,23 @@ let hide = function() {
   close.style.display = 'none';
 };
 
+//#4.1 - creating function
+let textChange = function() {
+  view.innerHTML = 'Hello, World!';
+};
+
+//#4.2 - crating function
+let textReturn = function () {
+  view.innerHTML = 'View';
+}
+
 view.onclick = open;
 close.onclick = hide;
+
+//#4.3 - creating event handler event
+view.addEventListener('click', textChange);
+
+//#4.4 - creating the close event handler event
+close.addEventListener('click', textReturn);
 
 // Write your code here
